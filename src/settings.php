632d0f59b07f91a95ade3d074b05187d6b64401e
Henrique Boaventura <hboavent@thoughtworks.com>
2019-01-16 10:39:55 -0200

    Using the new slim methods to simplify the logic

--- src/settings.php
+++ src/settings.php
@@ -4,16 +4,12 @@ return [
         'displayErrorDetails' => true, // set to false in production
         'addContentLengthHeader' => false, // Allow the web server to send the content-length header
 
-        // Renderer settings
-        'renderer' => [
-            'template_path' => __DIR__ . '/../templates/',
-        ],
-
         // Monolog settings
-        'logger' => [
-            'name' => 'slim-app',
-            'path' => __DIR__ . '/../logs/app.log',
-            'level' => \Monolog\Logger::DEBUG,
-        ],
+        'logger' => function($c) {
+            $logger = new \Monolog\Logger('bcjp-game');
+            $file_handler = new \Monolog\Handler\StreamHandler('../logs/app.log');
+            $logger->pushHandler($file_handler);
+            return $logger;
+        },
     ],
 ];
